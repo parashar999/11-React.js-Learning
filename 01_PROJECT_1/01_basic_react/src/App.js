0ef8e366d50fb430210ca56bb410c8b0b8b351b3
Tarun Sharma <83560354+sharmatarun392000@users.noreply.github.com>
2024-07-20 22:46:47 +0000

    01 : Learn - Project Cleaning in React and Vite

---
+++ 01_PROJECT_1/01_basic_react/src/App.js
@@ -0,0 +1,7 @@
+function App() {
+  return (
+    <h1>Tarun Parashar</h1>
+  );
+}
+
+export default App;
---
+++ 01_PROJECT_1/02_basic_vite_project/src/App.jsx
@@ -0,0 +1,10 @@
+
+function App() {
+  return (
+    <h1>
+     Tarun Parashar
+    </h1>
+  )
+}
+
+export default App
